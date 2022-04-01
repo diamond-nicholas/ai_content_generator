b04e8e0737d45dd002d610d046d35a4fbaaed7e5
diamond-nicholas <diamondnicholas154@gmail.com>
2022-04-01 17:54:36 +0100

    complete logic for ai to generate content for product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-const { Configuration, OpenAIAPI } = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
+// require('dotenv').config();
 
 const Product = () => {
   const [header, setHeader] = useState(
@@ -13,10 +14,30 @@ const Product = () => {
     productname: '',
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const { productname } = values;
     console.log(productname);
+    setHeader(`AI suggesition for ${productname}`);
+
+    ///open ai
+
+    const configuration = new Configuration({
+      // apiKey: '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',
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai
+      .createCompletion('text-davinci-002', {
+        prompt: `Write a detailed, smart, informative and professional product description for ${productname}`,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      .then((data) => setText(data.data.choices[0].text));
   };
 
   const handleChange = (e) => {
